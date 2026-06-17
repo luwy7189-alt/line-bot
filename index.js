788fc2e9e35641996404f9bd6817952cf81150fa
luwy7189-alt <luwy7189@gmail.com>
2026-06-17 22:17:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-const express = require("express");
-
-const app = express();
-
-app.use(express.json());
-
-// 首頁測試
-app.get("/", (req, res) => {
-  res.send("OK");
-});
-
-// LINE Webhook測試
 app.post("/webhook", (req, res) => {
-
-  console.log("🔥 WEBHOOK HIT");
-
-  console.log(
-    JSON.stringify(req.body, null, 2)
-  );
+  console.log("🔥 HIT FROM LINE");
+  console.log(req.headers);
+  console.log(req.body);
 
   return res.status(200).send("ok");
-
 });
-
-module.exports = app;
